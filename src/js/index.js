6fdad4e5046ee0c5ac7e6f3403d0562f96220d57
moonprincessdomi <moonprincessdomi@gmail.com>
2019-11-17 20:11:42 +0100

    Deleted start button; Added pawns on board with new game button click

--- src/js/index.js
+++ src/js/index.js
@@ -83,6 +83,8 @@ switch (dice_value) {
     }
 }
 
+const PLAYERS = ["green", "red", "yellow", "blue"];
+
 function loadBoard() {
     console.log("aaa");
     // for(let i= 1; i <=30; i++){
@@ -96,10 +98,36 @@ function loadBoard() {
     for(let i = 0; i <childBoards.length;i++){
         let childBoard = childBoards[i];
         childBoard.style.gridArea = childBoard.id;
-        console.log(childBoard.id);
-        console.log(childBoard.style.gridArea);
-
+        //console.log(childBoard.id);
+        //console.log(childBoard.style.gridArea);
     }
+
+    
+
+    // const pawns = {};
+    // PLAYERS.forEach(color => {
+    //     pawns[color] = [];
+    //     for(let i =1; i <=4; i++){
+    //         pawns[color].push(document.getElementById(`pawn${color}${i}`));
+    //     }
+    // });
+}
+
+
+function Start(){
+    console.log("start");
+    const pawns = PLAYERS.reduce((prev, color) => {
+        prev[color] = [];
+        for(let i =1; i <=4; i++){
+            prev[color].push(document.getElementById(`pawn${color}${i}`));
+            let newpawn = document.createElement("div");
+            newpawn.id = `mpawn${color}${i}`;
+            newpawn.className = `pawn ${color}field`;
+            newpawn.style.gridArea = `pawn${color}${i}`;
+            document.getElementById("gameboard").appendChild(newpawn);
+        }
+        return prev;
+    }, {});
 }
 
 var Init;
@@ -112,6 +140,6 @@ function InitStart(){
     Init =  setTimeout(loadBoard,3000);
 }
 
-//document.getElementsByTagName("body")[0].onload = InitStart;
+document.getElementsByTagName("body")[0].onload = InitStart;
 document.getElementById("roll").onclick = diceRoll;
-document.getElementById("start").onclick = InitStart;
+document.getElementById("newgame").onclick = Start;
